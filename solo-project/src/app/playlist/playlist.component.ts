24b47b1754058e52caba738128a77804c6fa395b
seriouslucy <seriouslucy@outlook.com>
2025-04-05 18:14:04 -0500

    modified
    fixing the styling a little, service edited

--- solo-project/src/app/playlist/playlist.component.ts
+++ solo-project/src/app/playlist/playlist.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, input } from '@angular/core';
 import { SongService } from '../shared/services/song.service';
 import { SongComponent } from './song/song.component';
 import { NewSongComponent } from "./new-song/new-song.component";
-import { Song } from '../shared/models/song.model';
+
 
 @Component({
   selector: 'app-playlist',
--- solo-project/src/app/shared/services/song.service.ts
+++ solo-project/src/app/shared/services/song.service.ts
@@ -8,9 +8,12 @@ export class SongService {
 
   songs = signal<Song[]>(
     [
-    {title: 'Silver Springs', artist: 'Fleetwood Mac', album: 'Single', year: 1977},
     {title: 'Miss Misery', artist: 'Elliott Smith', album: 'Single', year: 1998},
-    {title: 'Nude', artist: 'Radiohead', album: 'In Rainbows', year: 2007}
+    {title: 'Silver Springs', artist: 'Fleetwood Mac', album: 'Single', year: 1977},
+    {title: 'Nude', artist: 'Radiohead', album: 'In Rainbows', year: 2007},
+    {title: 'Bloodstream', artist: 'Stateless', album: 'Stateless', year: 2007},
+    {title: 'Traces', artist: 'Red Vox', album: 'Afterthoughts', year: 2023},
+    {title: 'Stylo', artist: 'Gorillaz', album: 'Plastic Beach', year: 2010}
     ]);
 
     getSongs() {
